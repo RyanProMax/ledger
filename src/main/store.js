65c0797020d894728d1ee26fc6d3622e233c2156
Ryan <627311610@qq.com>
2022-03-13 19:11:35 +0800

    fix: storage name

--- src/main/store.js
+++ src/main/store.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fse = require('fs-extra');
 const { Low, JSONFile } = require('lowdb-node');
 const { AppInfo } = require('./app');
-const { DEFAULT_DATA, STORE_NAME } = require('./constant');
+const { DEFAULT_DATA, STORE_NAME, STORE_FILE_NAME } = require('./constant');
 
 let STORE_PATH;
 
@@ -11,7 +11,7 @@ let STORE_PATH;
  * STORE_NAME的属性名须与DEFAULT_DATA的保持一致对应
  */
 const initStoreData = async (storeName) => {
-  const file = path.resolve(STORE_PATH, STORE_NAME[storeName]);
+  const file = path.resolve(STORE_PATH, STORE_FILE_NAME[storeName]);
   const adapter = new JSONFile(file);
   const db = new Low(adapter);
   await db.read();
@@ -20,21 +20,8 @@ const initStoreData = async (storeName) => {
 };
 
 /**
- * @description 获取数据库数据
+ * @description 初始化数据库
  */
-const getStoreData = async (storeName) => {
-  try {
-    const file = path.resolve(STORE_PATH, STORE_NAME[storeName]);
-    const adapter = new JSONFile(file);
-    const db = new Low(adapter);
-    await db.read();
-    db.data = db.data || DEFAULT_DATA[storeName];
-    return { status: 0, error: null, data: db.data };
-  } catch (e) {
-    return { status: 1, error: e.message, data: null };
-  }
-};
-
 const initStore = async () => {
   const appInfo = AppInfo.getInstance();
   const USER_DATA_PATH = appInfo.constant.get('USER_DATA_PATH');
@@ -46,9 +33,28 @@ const initStore = async () => {
   await Promise.all(initStoreDataList.map((name) => initStoreData(name)));
 };
 
+/**
+ * @description 获取数据库数据
+ */
+const getStoreData = async (storeName) => {
+  try {
+    const file = path.resolve(STORE_PATH, STORE_FILE_NAME[storeName]);
+    const adapter = new JSONFile(file);
+    const db = new Low(adapter);
+    await db.read();
+    db.data = db.data || DEFAULT_DATA[storeName];
+    return { status: 0, error: null, data: db.data };
+  } catch (e) {
+    return { status: 1, error: e.message, data: null };
+  }
+};
+
+/**
+ * @description 设置数据库数据
+ */
 const setStoreData = async ({ storeName, data }) => {
   try {
-    const file = path.resolve(STORE_PATH, STORE_NAME[storeName]);
+    const file = path.resolve(STORE_PATH, STORE_FILE_NAME[storeName]);
     const adapter = new JSONFile(file);
     const db = new Low(adapter);
     await db.read();
